f66fb294d9f3c62fd049beca059202deb2ea8c98
Dani Marín <danii04.mb@gmail.com>
2023-05-30 00:02:42 +0200

    Add Create Prompt View and funcionality

--- utils/database.js
+++ utils/database.js
@@ -6,7 +6,7 @@ export const connectToDB = async () => {
   mongoose.set('strictQuery', true);
 
   if (isConnected) {
-    console.log('MongoDB is alredy connexted');
+    console.log('MongoDB is already connexted');
     return;
   }
 
