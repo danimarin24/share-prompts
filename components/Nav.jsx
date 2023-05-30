1ec0628dc70f8fbcad7f3824d178deef6c566c26
Dani Marín <danii04.mb@gmail.com>
2023-05-30 12:19:05 +0200

    Change Logo Name

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -29,7 +29,7 @@ const Nav = () => {
           height={30}
           className='object-contain'
         />
-        <p className='logo_text'>Promptopia</p>
+        <p className='logo_text'>Share Prompts</p>
       </Link>
 
       {/* Desktop Navigation */}
